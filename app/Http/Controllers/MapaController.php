6ccae8530e9617135c50dddb9f63ca0baf25f11c
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2017-11-28 05:52:42 -0400

    mapa cargado para poligono

--- app/Http/Controllers/MapaController.php
+++ app/Http/Controllers/MapaController.php
@@ -14,6 +14,25 @@ class MapaController extends Controller
     public function getMapa(Request $request, $id){
         $id = decrypt($id);
         $coordenada = Coordenada::where('idobjetivo','=',$id)->get();
-        return view('admin.objetivo.mapa');
+        $poligono = array();
+        $mostrar = 0;
+        if(count($coordenada)>0){
+            $mostrar = 1;
+            foreach($coordenada as $item){
+                $poligono[] = ['latitude' => $item->coor_x_map, 'longitude' => $item->coor_y_map];
+            }
+            Mapper::map(
+                $poligono[0]['latitude'],$poligono[0]['longitude'],
+                ['zoom' => 16, 'center' => true, 'marker' => true, 'overlay' => 'TRAFFIC', 'animation' => 'BOUNCE']
+                )
+                ->polygon($poligono, 
+                [
+                    'strokeColor' => '#047C36', 
+                    'strokeOpacity' => 0.3, 
+                    'strokeWeight' => 6, 
+                    'fillColor' => '#09AF4F',
+                ]);
+        }
+        return view('admin.objetivo.mapa')->with('mostrar', $mostrar);
     }
 }
--- resources/views/admin/objetivo/mapa.blade.php
+++ resources/views/admin/objetivo/mapa.blade.php
@@ -1,19 +1,9 @@
 <div style="widht:100%; height:100%">
-<?php
-    Mapper::map(    
-            //traer coordenas del objetivo especifico
-            -16.5191841,
-            -68.1685108,
-            [
-                'zoom' => 16,
-                'overlay' => 'TRAFFIC',
-                //tipos de animation:NONE, DROP, BOUNCE
-                //'markers' => ['title' => 'My Location','content' => 'Mi accion', 'animation' => 'BOUNCE'],
-                'markers' => ['animation' => 'BOUNCE'],
-                'draggable' => false, //para activar o desactivar para que se mueva el marcador
-                'marker' => true        //para posicionar el marcador segun coordenadas
-            ]
-        );
-?>
-{!! Mapper::render() !!}
+@if($mostrar != 0)
+    {!! Mapper::render() !!}
+@else
+    <center>
+        <h2 style="font-family:arial;color:green;">Debe registrar las Coordenadas Correspondientes para ver el polígono</h2>
+    </center>
+@endif
 </div>
